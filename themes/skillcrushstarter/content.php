79b753a5880b06d1ec2195baf197debe9fb1a6d9
tauri_s <tauristclaire@gmail.com>
2025-05-08 15:01:31 -0700

    Remove classes and other entry-meta div tag not attached to styles

--- themes/skillcrushstarter/content.php
+++ themes/skillcrushstarter/content.php
@@ -13,16 +13,14 @@
 <article id="post-<?php the_ID(); ?>" class="post-entry">
 	<div class="entry-wrap">
 		<header class="entry-header">
-			<div class="entry-meta">
-				<time class="entry-time"><?php echo get_the_date(); ?></time>
-			</div>
+			<time class="entry-time"><?php echo get_the_date(); ?></time>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		<div class="entry-summary">
 			<?php the_content(); ?>
 		</div>
 		<footer class="entry-footer">
-			<span class="entry-terms comments author">
+			<span class="entry-terms">
 				Written by <?php the_author(); ?> / 
 				Posted in <?php the_category(', '); ?> /
 				<a href="<?php comments_link(); ?>"><?php comments_number( '<span>No Comments</span>', '1 Comment', '% comments' ); ?></a>
